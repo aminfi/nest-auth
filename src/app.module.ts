d14be1899ca24a3986b8c0c13c0384cec7c1876b
Mohammad Amin Faraji <amin_fi@ymail.com>
2020-02-03 09:26:41 +0330

    chore: add prettier config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
 export class AppModule {}
